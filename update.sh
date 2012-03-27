442c48f4dda0709fc3dbba3e50738ebabd2f1dbf
Eddie Kao <eddie@digik.com.tw>
2012-03-27 12:09:56 +0800

    update the update script

--- update.sh
+++ update.sh
@@ -4,3 +4,9 @@ git submodule init
 git submodule update
 git submodule foreach git submodule init
 git submodule foreach git submodule update
+#git submodule foreach git pull origin master
+
+git submodule foreach '[ "$path" = "bundle/vim-powerline" ] && branch=develop \
+  || branch=master; git checkout $branch'
+# special case
+
