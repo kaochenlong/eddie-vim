133c8272ce50ab59582b69b7d6838ed1c690cceb
Eddie Kao <eddie@digik.com.tw>
2012-07-17 05:21:17 +0800

    update the `update script`

--- update.sh
+++ update.sh
@@ -4,6 +4,7 @@ git submodule init
 git submodule update
 git submodule foreach git submodule init
 git submodule foreach git submodule update
+git submodule foreach git pull
 #git submodule foreach git pull origin master
 
 # special case
